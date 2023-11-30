09ac877edcfd17b7eec7240bd0cc1b4e3620595a
Antoine Buquet <61704871+antoinebqt@users.noreply.github.com>
2023-11-30 15:34:57 +0100

    better dual check alarm script groovy

--- embeddeds/groovy/GroovuinoML/scripts/Dual_Check_Alarm.groovy
+++ embeddeds/groovy/GroovuinoML/scripts/Dual_Check_Alarm.groovy
@@ -4,10 +4,16 @@ actuator "buzzer1" pin 12
 
 state "on" means "buzzer1" becomes "high"
 state "off" means "buzzer1" becomes "low"
+state "button1_on"
+state "button2_on"
 
 initial "off"
 
+from "off" to "button1_on" when "button1" becomes "high"
+from "off" to "button2_on" when "button2" becomes "high"
 from "off" to "on" when "button1" becomes "high" and "button2" becomes "high"
+from "button1_on" to "off" when "button1" becomes "low"
+from "button2_on" to "off" when "button2" becomes "low"
 from "on" to "off" when "button1" becomes "low" or "button2" becomes "low"
 
 export "Dual-check alarm"
