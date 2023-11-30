ec031a159a85673f5ed2c632cd342e519e6c2790
Antoine Buquet <61704871+antoinebqt@users.noreply.github.com>
2023-11-30 16:35:38 +0100

    fix switch example

--- embeddeds/groovy/GroovuinoML/scripts/Switch.groovy
+++ embeddeds/groovy/GroovuinoML/scripts/Switch.groovy
@@ -1,10 +1,9 @@
 sensor "button1" pin 9
 actuator "led1" pin 12
 actuator "led2" pin 13
-actuator "led3" pin 14
 
 state "on" means "led1" becomes "high"
-state "off" means "led1" becomes "low" and "led2" becomes "low" and "led3" becomes "low"
+state "off" means "led1" becomes "low" and "led2" becomes "low"
 
 initial "off"
 
