80f69888565ce144b114749878b3589cea5a37f2
Benoit Gaudet <bgaudet38@gmail.com>
2023-12-02 18:08:28 +0100

    Extension groovy with LCD screen

--- embeddeds/groovy/GroovuinoML/scripts/Simple_LCD.groovy
+++ embeddeds/groovy/GroovuinoML/scripts/Simple_LCD.groovy
@@ -4,13 +4,10 @@ actuatorLCD "screen" bus 1
 actuator "snooze1" pin 11
 actuator "led1" pin 12
 
-state "both" means "led1" becomes "high" and "snooze1" becomes "high"
-state "led" means "led1" becomes "high" and "snooze1" becomes "low"
-state "snooze" means "led1" becomes "low" and "snooze1" becomes "high"
-state "none" means "led1" becomes "low" and "snooze1" becomes "low"
-
-//state "on" means "screen" display "Mouradox UwU"
-//state "off" means "screen" hide ""
+state "both" means "led1" becomes "high" and "snooze1" becomes "high" andLCD "screen" display "Led : ON" row 0 andLCD "screen" display "Snooze : ON" row 1
+state "led" means "led1" becomes "high" and "snooze1" becomes "low" andLCD "screen" display "Led : ON" row 0 andLCD "screen" display "Snooze : OFF" row 1
+state "snooze" means "led1" becomes "low" and "snooze1" becomes "high" andLCD "screen" display "Led : OFF" row 0 andLCD "screen" display "Snooze : ON" row 1
+state "none" means "led1" becomes "low" and "snooze1" becomes "low" andLCD "screen" display "Led : OFF" row 0 andLCD "screen" display "Snooze : OFF" row 1
 
 initial "none"
 
